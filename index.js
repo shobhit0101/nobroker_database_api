9a71fe4c978711d306fa65955515656d3210d5da
shobhit0101 <shobhitgpt01@gmail.com>
2022-12-05 23:13:15 +0530

    checking

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const jsonServer = require("json-server");
+const data = require('./db.json');
 const server = jsonServer.create();
-const router = jsonServer.router(db.json);
+const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 const port = process = process.env.PORT || 3001;
 server.use(middlewares);
